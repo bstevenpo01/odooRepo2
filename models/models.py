2e321fac33e54fbf39cf7329acf606735c97cbc8
bstevenpo01 <91967732+bstevenpo01@users.noreply.github.com>
2022-03-22 20:39:46 +0100

    cambios

--- models/models.py
+++ models/models.py
@@ -25,7 +25,7 @@ class incidencia(models.Model):
     _name = 'incidencias.incidencia'
     _description = 'Define los atributos de una incidencia'
     
-    motivoIncidencia = fields.Selection(string='Motivo de la incidencia', selelection=[('a','Funcionamiento'),('b','Jugabilidad'),('c','Online'),('d','Multijugador')], required=True)
+    motivoIncidencia = fields.Selection(string='Motivo de la incidencia', selection=[('a','Funcionamiento'),('b','Jugabilidad'),('c','Online'),('d','Multijugador')], required=True)
     descripcionIncidencia = fields.Text(string='descripcion de la incidencia', required=True, help='Escribe una descripcion detallada')
     fechaIncidencia = fields.Date(string='Fecha de la incidencia', required=True, default=fields.Date.today())
 
