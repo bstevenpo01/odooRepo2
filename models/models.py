7200957907b14c9d8aee375e13320854f83a479a
bstevenpo01 <91967732+bstevenpo01@users.noreply.github.com>
2022-03-22 14:45:49 +0100

    creacion del modelo

--- models/models.py
+++ models/models.py
@@ -18,3 +18,14 @@
 #             record.value2 = float(record.value) / 100
 
 
+class incidencia(models.Model):
+    _name = 'incidencias.incidencia'
+    _description = 'Define los atributos de una incidencia'
+
+    motivoIncidencia = fields.Selection(string='Motivo de la incidencia', selelection=[('a','Funcionamiento'),('b','Jugabilidad'),('c','Online'),('d','Multijugador')], required=True)
+
+    descripcionIncidencia = fields.Text(string='descripcion de la incidencia' required=True, help='Escribe una descripcion detallada')
+    fechaIncidencia = fields.Date(string='Fecha de la incidencia', required=True, default=fields.Date.today())
+
+    #Relacion entre tablas
+    cliente_id = fields.One2many('alquileres.cliente', 'cliente.incidencia_id')
